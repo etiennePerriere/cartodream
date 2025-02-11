c0ef59883268288b4c570e73c582fef7287d7d02
Etienne Perriere <etienne.perriere@gmail.com>
2025-02-11 01:11:51 +0100

    Categories panels desktop. WIP : search & mobile

--- js/components/filters.js
+++ js/components/filters.js
@@ -1,188 +1,306 @@
 var FilterComponent = {
-    tabCategories: new Array(),
-    tabCategories2: new Array(),
-    poiLayers: new Array(),
+    categories: {},                   // Categories and their subcategories
+    selectedSubCategories: new Set(), // Currently selected subcategories
+    currentCategory: null,           // Currently displayed category
+    poiLayers: new Array(),         // Store map markers/features
 
     initialize: function() {
         // First clear any existing markers
         markers.clearLayers();
         map.removeLayer(markers);
         
-        // Process features and create filter lists
+        // Process data and setup UI
         this.processFeatures();
         this.createFilterLists();
+        this.setupEventListeners();
         
         // Add markers to map after all processing is done
         map.addLayer(markers);
     },
 
+    // Data Processing Methods
     processFeatures: function() {
-        for (var i = 0; i < mapPOIs.features.length; i++) {
-            var currentFeature = mapPOIs.features[i];
-            
-            // Categories initialization
-            if (this.tabCategories[currentFeature.properties.categorie] == null) {
-                this.tabCategories[currentFeature.properties.categorie] = new Array();
-            }
-            
-            if (currentFeature.properties.sous_cat != null) {
-                this.tabCategories[currentFeature.properties.categorie][currentFeature.properties.sous_cat] = true;
-            } else {
-                this.tabCategories[currentFeature.properties.categorie] = true;
-            }
+        if (!mapPOIs || !mapPOIs.features) return;
 
-            // Categories2 initialization (for icons)
-            if (this.tabCategories2[currentFeature.properties.categorie] == null) {
-                this.tabCategories2[currentFeature.properties.categorie] = new Array();
-            }
-            if (currentFeature.properties.sous_cat != null) {
-                this.tabCategories2[currentFeature.properties.categorie][currentFeature.properties.etape] = true;
-            } else {
-                this.tabCategories2[currentFeature.properties.categorie] = true;
+        mapPOIs.features.forEach(feature => {
+            const category = feature.properties.categorie;
+            const subCategory = feature.properties.sous_cat;
+
+            // Process categories and subcategories
+            if (category) {
+                if (!this.categories[category]) {
+                    this.categories[category] = new Set();
+                }
+                if (subCategory) {
+                    this.categories[category].add(subCategory);
+                }
             }
 
             // Layer creation
-            this.poiLayers[currentFeature.properties.id] = {
-                "feature": currentFeature,
+            this.poiLayers[feature.properties.id] = {
+                "feature": feature,
                 "marker": MapComponent.createMarker(
-                    currentFeature, 
-                    L.latLng(currentFeature.geometry.coordinates[1], currentFeature.geometry.coordinates[0])
+                    feature, 
+                    L.latLng(feature.geometry.coordinates[1], 
+                            feature.geometry.coordinates[0])
                 )
             };
 
             // Create and bind popup
-            if (typeof(this.poiLayers[currentFeature.properties.id].marker) != "undefined") {
-                var popupContent = PopupComponent.createContent(currentFeature);
-                this.poiLayers[currentFeature.properties.id].marker.bindPopup(
+            if (typeof(this.poiLayers[feature.properties.id].marker) != "undefined") {
+                var popupContent = PopupComponent.createContent(feature);
+                this.poiLayers[feature.properties.id].marker.bindPopup(
                     popupContent,
                     siteConfig.popup.options
                 );
                 
-                PopupComponent.setupEvents(currentFeature, this.poiLayers[currentFeature.properties.id].marker);
-                markers.addLayer(this.poiLayers[currentFeature.properties.id].marker);
+                PopupComponent.setupEvents(feature, 
+                    this.poiLayers[feature.properties.id].marker);
+                markers.addLayer(this.poiLayers[feature.properties.id].marker);
             }
-        }
+        });
     },
 
+    // UI Methods
     createFilterLists: function() {
-        var selectCategories = '';
-        for (var i in this.tabCategories) {
-            for (var j in this.tabCategories[i]) {
-                selectCategories += 
-                    '<div class="cell filter-item">' +
-                        '<div class="filter-item-content">' +
-                            '<input type="checkbox" id="' + j + '" class="custom-checkbox"' +
-                            'checked="true" onclick="FilterComponent.onDisplayCheckBoxChanged(\'' + j + '\', \'' + i + '\');">' +
-                            '<label for="' + j + '" class="custom-label">' + j + '</label>' +
-                        '</div>' +
-                    '</div>';
-            }
+        // Create categories grid
+        const categoriesContainer = document.querySelector('.categories-panel .grid-x');
+        if (categoriesContainer) {
+            categoriesContainer.innerHTML = Object.keys(this.categories).map(category => `
+                <div class="cell" data-category="${category}">
+                    <div class="category-item">
+                        <img src="images/icons/${mapConfig.formatIconName(category)}.png" 
+                             alt="${category}" 
+                             class="category-icon">
+                        <span>${category}</span>
+                    </div>
+                </div>
+            `).join('');
         }
-        $('#select-list').html(selectCategories);
-        $('#select-list-mobile').html(selectCategories);
-
-        var selectCategories2 = '';
-        for (var i in this.tabCategories2) {
-            for (var j in this.tabCategories2[i]) {
-                selectCategories2 += 
-                    '<div class="cell auto filter-type-item">' +
-                        '<label class="actor-type-filter">' +
-                            '<input type="checkbox" class="visually-hidden actor-type-checkbox" ' +
-                                'checked ' +
-                                'data-type="' + j + '" ' +
-                                'data-original-icon="' + mapConfig.formatIconName(j) + '" ' +
-                                'onclick="FilterComponent.handleActorTypeChange(this);">' +
-                            '<div class="actor-type-content">' +
-                                '<img src="images/icons/' + mapConfig.formatIconName(j) + '.png" alt="' + j + '">' +
-                                '<span>' + j + '</span>' +
-                            '</div>' +
-                        '</label>' +
-                    '</div>';
-            }
-        }
-        $('#select-list2').html(selectCategories2);
-        $('#select-list2-mobile').html(selectCategories2);
     },
 
-    handleActorTypeChange: function(checkbox) {
-        var $label = $(checkbox).closest('.actor-type-filter');
-        var $img = $label.find('img');
+    setupEventListeners: function() {
+        // Category clicks
+        document.querySelectorAll('.category-item').forEach(item => {
+            item.addEventListener('click', (e) => {
+                const category = e.currentTarget.closest('[data-category]').dataset.category;
+                this.showSubCategories(category);
+            });
+        });
+
+        // Back button
+        const backBtn = document.querySelector('.back-to-categories');
+        if (backBtn) {
+            backBtn.addEventListener('click', () => this.showCategories());
+        }
+
+        // Select all button
+        const selectAllBtn = document.querySelector('.select-all-button');
+        if (selectAllBtn) {
+            selectAllBtn.addEventListener('click', () => this.selectAllSubCategories());
+        }
+
+        // Remove all selected
+        const removeAllBtn = document.querySelector('.remove-all');
+        if (removeAllBtn) {
+            removeAllBtn.addEventListener('click', () => this.removeAllSelected());
+        }
+
+        // Mobile filter button
+        const filterBtn = document.querySelector('.filter-button');
+        if (filterBtn) {
+            filterBtn.addEventListener('click', () => this.toggleMobileFilters());
+        }
+
+        // Search related
+        const searchInputs = document.querySelectorAll('.search-container input');
+        const regularSearch = document.querySelector('[data-panel="regular-search"]');
+        const focusedSearch = document.querySelector('[data-panel="focused-search"]');
+
+        searchInputs.forEach(input => {
+            // Handle focus
+            input.addEventListener('focus', () => {
+                regularSearch.classList.add('hide');
+                focusedSearch.classList.remove('hide');
+                focusedSearch.querySelector('input').focus()
+            });
+            
+            // Handle blur
+            input.addEventListener('blur', (e) => {
+                setTimeout(() => {
+                    const stillInSearch = document.activeElement.closest('.search-container') || document.activeElement.closest('.search-results');
+                
+                    if (!stillInSearch) {
+                        focusedSearch.classList.add('hide');
+                        regularSearch.classList.remove('hide');
+                    }
+                }, 100);
+            });
+        });
+    },
+
+    showCategories: function() {
+        document.querySelector('.logo-search-panel').classList.remove('hide');
+        document.querySelector('.categories-panel').classList.remove('hide');
+        document.querySelector('.subcategories-panel').classList.add('hide');
+    },
+
+    showSubCategories: function(category) {
+        this.currentCategory = category;
         
-        if (checkbox.checked) {
-            $img.attr('src', 'images/icons/' + $(checkbox).data('original-icon') + '.png');
+        document.querySelector('.logo-search-panel').classList.add('hide');
+        document.querySelector('.categories-panel').classList.add('hide');
+        document.querySelector('.subcategories-panel').classList.remove('hide');
+
+        const container = document.querySelector('#subcategories-list');
+        if (container) {
+            container.innerHTML = Array.from(this.categories[category]).map(subCat => `
+                <div class="cell subcategory-item" data-subcategory="${subCat}">
+                    <div class="grid-x align-middle grid-padding-x">
+                        <div class="cell shrink">
+                            <img src="images/icons/${mapConfig.formatIconName(subCat)}.png" 
+                                alt="${subCat}" 
+                                class="subcategory-icon">
+                        </div>
+                        <div class="cell auto">
+                            ${subCat}
+                        </div>
+                        <div class="cell shrink">
+                            <div class="custom-checkbox">
+                                <input type="checkbox" 
+                                    id="check_${subCat}" 
+                                    ${this.selectedSubCategories.has(subCat) ? 'checked' : ''}>
+                                <label for="check_${subCat}"></label>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            `).join('');
+
+            container.querySelectorAll('.subcategory-item').forEach(item => {
+                item.addEventListener('click', (e) => {
+                    const subCat = item.dataset.subcategory;
+                    const checkbox = item.querySelector('input[type="checkbox"]');
+                    checkbox.checked = !checkbox.checked;
+                    this.toggleSubCategory(subCat, checkbox.checked);
+                });
+            });
+        }
+
+        this.updateSelectedPanel();
+    },
+
+    toggleSubCategory: function(subCategory, selected) {
+        if (selected) {
+            this.selectedSubCategories.add(subCategory);
         } else {
-            $img.attr('src', 'images/icons/aucun.png');
+            this.selectedSubCategories.delete(subCategory);
         }
-        
-        this.updateDisplayByType();
-    },
-
-    updateDisplayByType: function() {
-        // Get unique active types (unchecked boxes types should be excluded)
-        var activeTypes = Array.from(new Set(
-            $('.actor-type-checkbox:not(:checked)').map(function() {
-                return $(this).data('type');
-            }).get()
-        ));
-    
-        map.removeLayer(markers);
-        markers.clearLayers();
-    
-        for (var idLayer in this.poiLayers) {
-            var currentLayer = this.poiLayers[idLayer];
-            var featureType = currentLayer.feature.properties.etape;
-    
-            // If type is in activeTypes, it should be excluded
-            var shouldExclude = activeTypes.indexOf(featureType) !== -1;
-            var categoryActive = this.isCategoryActive(currentLayer.feature);
-    
-            if (!shouldExclude && categoryActive) {
-                markers.addLayer(currentLayer.marker);
-            }
-        }
-    
-        map.addLayer(markers);
-    },
-
-    onDisplayCheckBoxChanged: function(idChangedElement, parentCategoryId) {
-        this.tabCategories[parentCategoryId][idChangedElement] = document.getElementById(idChangedElement).checked;
+        this.updateSelectedPanel();
         this.updateDisplay();
+    },
+
+    selectAllSubCategories: function() {
+        if (!this.currentCategory) return;
+        
+        this.categories[this.currentCategory].forEach(subCat => {
+            this.selectedSubCategories.add(subCat);
+        });
+        
+        const checkboxes = document.querySelectorAll('#subcategories-list input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = true;
+        });
+        
+        this.updateSelectedPanel();
+        this.updateDisplay();
+    },
+
+    removeAllSelected: function() {
+        if (!this.currentCategory) return;
+        
+        this.categories[this.currentCategory].forEach(subCat => {
+            this.selectedSubCategories.delete(subCat);
+        });
+        
+        const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+        checkboxes.forEach(checkbox => {
+            checkbox.checked = false;
+        });
+        
+        this.updateSelectedPanel();
+        this.updateDisplay();
+    },
+
+    updateSelectedPanel: function() {
+        const panel = document.querySelector('.selected-subcategories-panel');
+        const container = document.querySelector('#selected-subcategories-list');
+        if (!container || !panel) return;
+
+        // Show/hide panel based on selection
+        if (this.selectedSubCategories.size > 0) {
+            panel.classList.remove('hide');
+        } else {
+            panel.classList.add('hide');
+        }
+
+        container.innerHTML = Array.from(this.selectedSubCategories).map(subCat => `
+            <div class="cell shrink">
+                <div class="selected-subcategory" data-subcategory="${subCat}">
+                    <img src="images/icons/${mapConfig.formatIconName(subCat)}.png" 
+                         alt="${subCat}">
+                    <button class="remove-subcategory" data-subcategory="${subCat}">
+                        <i class="fa-solid fa-xmark"></i>
+                    </button>
+                </div>
+            </div>
+        `).join('');
+
+        container.querySelectorAll('.remove-subcategory').forEach(btn => {
+            btn.addEventListener('click', (e) => {
+                const subCat = e.currentTarget.dataset.subcategory;
+                this.toggleSubCategory(subCat, false);
+                const checkbox = document.querySelector(`#check_${subCat}`);
+                if (checkbox) {
+                    checkbox.checked = false;
+                }
+            });
+        });
     },
 
     updateDisplay: function() {
         map.removeLayer(markers);
+        markers.clearLayers();
+
         for (var idLayer in this.poiLayers) {
             var currentLayer = this.poiLayers[idLayer];
-            if (currentLayer.feature.properties.sous_cat != null) {
-                if (this.tabCategories[currentLayer.feature.properties.categorie][currentLayer.feature.properties.sous_cat] == true) {
-                    if (!markers.hasLayer(currentLayer.marker)) {
-                        markers.addLayer(currentLayer.marker);
-                    }
-                } else {
-                    if (markers.hasLayer(currentLayer.marker)) {
-                        markers.removeLayer(currentLayer.marker);
-                    }
-                }
-            } else {
-                if (this.tabCategories[currentLayer.feature.properties.categorie] == true) {
-                    if (!markers.hasLayer(currentLayer.marker)) {
-                        markers.addLayer(currentLayer.marker);
-                    }
-                } else {
-                    if (markers.hasLayer(currentLayer.marker)) {
-                        markers.removeLayer(currentLayer.marker);
-                    }
-                }
+            if (this.isLayerVisible(currentLayer)) {
+                markers.addLayer(currentLayer.marker);
             }
         }
+        
         map.addLayer(markers);
     },
-    
-    isCategoryActive: function(feature) {
-        if (feature.properties.sous_cat != null) {
-            return this.tabCategories[feature.properties.categorie][feature.properties.sous_cat];
+
+    isLayerVisible: function(layer) {
+        const subCategory = layer.feature.properties.sous_cat;
+        return this.selectedSubCategories.size === 0 || this.selectedSubCategories.has(subCategory);
+    },
+
+    // Mobile Methods
+    toggleMobileFilters: function() {
+        const header = document.querySelector('.mobile-header');
+        const filterBtn = document.querySelector('.filter-button');
+        const panel = document.querySelector('#mobile-filter-panel');
+
+        if (panel.classList.contains('is-open')) {
+            panel.classList.remove('is-open');
+            header.classList.remove('hide');
+            filterBtn.classList.remove('hide');
         } else {
-            return this.tabCategories[feature.properties.categorie];
+            panel.classList.add('is-open');
+            header.classList.add('hide');
+            filterBtn.classList.add('hide');
         }
     }
 };
